0209e47d77e27fc6a1db87e7d3282f3a442a0b4f
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2019-05-01 09:46:59 +0200

    Restructured code.

--- www/JavaScript.js
+++ www/JavaScript.js
@@ -6,12 +6,12 @@ const BOARD_SIZE_X = 5;
 const BOARD_SIZE_Y = 9;
 
 class Side{
-	constructor(color=""){
+	constructor(color=''){
 		this.colorString = color;
 	}
 }
 class PieceClass{
-	constructor(name="", reachStraight=0, reachDiagonally=0){
+	constructor(name='', reachStraight=0, reachDiagonally=0){
 		this.name = name;
 		this.reachStraight = reachStraight;
 		this.reachDiagonally = reachDiagonally;
@@ -45,13 +45,13 @@ function onload(){
 	colorWhite = new Color('#FFF');
 	colorBlack = new Color('#000');
 
-	let sideRed = new Side("Red");
+	let sideRed = new Side('Red');
 	players.push(sideRed);
-	let sideYellow = new Side("Yellow");
+	let sideYellow = new Side('Yellow');
 	players.push(sideYellow);
 
-	let classKing = new PieceClass("King", 2, 1);
-	let classQueen = new PieceClass("Queen", 4, 3);
+	let classKing = new PieceClass('King', 2, 1);
+	let classQueen = new PieceClass('Queen', 4, 3);
 	pieces.push(new Piece(classKing, sideRed, new Position(2, BOARD_SIZE_Y)));
 	pieces.push(new Piece(classQueen, sideRed, new Position(4, BOARD_SIZE_Y)));
 	pieces.push(new Piece(classKing, sideYellow, new Position(1, 1)));
@@ -71,30 +71,6 @@ function moveEvent(event){
 		let cell = new Position(x, y);
 		highlightedCells.push(cell);
 	}
-}
-function highlightCell(cell=new Position()){
-	let cellPixelPos = new Position(CELL_SIZE*(cell.X-1), CELL_SIZE*(cell.Y-1));
-
-	let sides = [
-		[new Position(cellPixelPos.X, cellPixelPos.Y), new Position(cellPixelPos.X, cellPixelPos.Y + CELL_SIZE)],
-		[new Position(cellPixelPos.X, cellPixelPos.Y + CELL_SIZE), new Position(cellPixelPos.X + CELL_SIZE, cellPixelPos.Y + CELL_SIZE)],
-		[new Position(cellPixelPos.X + CELL_SIZE, cellPixelPos.Y + CELL_SIZE), new Position(cellPixelPos.X + CELL_SIZE, cellPixelPos.Y)],
-		[new Position(cellPixelPos.X + CELL_SIZE, cellPixelPos.Y), new Position(cellPixelPos.X, cellPixelPos.Y)]
-	];
-
-	canvasContext.lineWidth = 5;
-	sides.forEach((side, index) => {
-		let offsetValue = ((Date.now() + CELL_SIZE*index)%(CELL_SIZE*40))/10;
-		var gradient = canvasContext.createLinearGradient(side[0].X + offsetValue, side[0].Y + offsetValue, side[1].X + offsetValue, side[1].Y + offsetValue);
-		gradient.addColorStop(0, "Red");
-		gradient.addColorStop(1, "transparent");
-
-		canvasContext.strokeStyle = gradient;
-		canvasContext.beginPath();
-		canvasContext.moveTo(side[0].X, side[0].Y);
-		canvasContext.lineTo(side[1].X, side[1].Y);
-		canvasContext.stroke();
-	});
 }
 function getEventPos(event, raw=false){
 	if(raw){return new Position(event.offsetX, event.offsetY);}
@@ -124,6 +100,15 @@ function getEventPos(event, raw=false){
 }
 function redrawBoard(){
 	canvasContext.clearRect(0, 0, BOARD_SIZE_X*CELL_SIZE, BOARD_SIZE_Y*CELL_SIZE);
+
+	drawCheckerboard();
+	drawPieces();
+	drawHighlightedCells();
+
+	animationStack.add(redrawBoard);
+}
+function drawCheckerboard(){
+	canvasContext.lineWidth = 1;
 	for(let index_x = 0; index_x < BOARD_SIZE_X; index_x++){
 		for(let index_y = 0; index_y < BOARD_SIZE_Y; index_y++){
 			if((index_x+index_y)%2 == 0){
@@ -135,7 +120,8 @@ function redrawBoard(){
 			canvasContext.fillRect(CELL_SIZE*index_x, CELL_SIZE*index_y, CELL_SIZE, CELL_SIZE);
 		}
 	}
-
+}
+function drawPieces(){
 	let baselineOffset = CELL_SIZE/10;
 	canvasContext.font = CELL_SIZE+'px Arial';
 	canvasContext.strokeStyle = colorBlack.toString();
@@ -148,7 +134,31 @@ function redrawBoard(){
 		canvasContext.fillText(symbol, (piece.position.X-.5)*CELL_SIZE, (piece.position.Y-.5)*CELL_SIZE + baselineOffset);
 		canvasContext.strokeText(symbol, (piece.position.X-.5)*CELL_SIZE, (piece.position.Y-.5)*CELL_SIZE + baselineOffset);
 	});
+}
+function drawHighlightedCells(){
+	let now = Date.now();
+	highlightedCells.forEach(cell => {
+		let cellPixelPos = new Position(CELL_SIZE*(cell.X-1), CELL_SIZE*(cell.Y-1));
 
-	highlightedCells.forEach(cell => highlightCell(cell));
-	animationStack.add(redrawBoard);
+		let sides = [
+			[new Position(cellPixelPos.X, cellPixelPos.Y), new Position(cellPixelPos.X, cellPixelPos.Y + CELL_SIZE)],
+			[new Position(cellPixelPos.X, cellPixelPos.Y + CELL_SIZE), new Position(cellPixelPos.X + CELL_SIZE, cellPixelPos.Y + CELL_SIZE)],
+			[new Position(cellPixelPos.X + CELL_SIZE, cellPixelPos.Y + CELL_SIZE), new Position(cellPixelPos.X + CELL_SIZE, cellPixelPos.Y)],
+			[new Position(cellPixelPos.X + CELL_SIZE, cellPixelPos.Y), new Position(cellPixelPos.X, cellPixelPos.Y)]
+		];
+	
+		canvasContext.lineWidth = 5;
+		sides.forEach((side, index) => {
+			let offsetValue = ((now + CELL_SIZE*index)%(CELL_SIZE*40))/10;
+			var gradient = canvasContext.createLinearGradient(side[0].X + offsetValue, side[0].Y + offsetValue, side[1].X + offsetValue, side[1].Y + offsetValue);
+			gradient.addColorStop(0, 'Red');
+			gradient.addColorStop(1, 'rgba(0,0,0,0)');
+	
+			canvasContext.strokeStyle = gradient;
+			canvasContext.beginPath();
+			canvasContext.moveTo(side[0].X, side[0].Y);
+			canvasContext.lineTo(side[1].X, side[1].Y);
+			canvasContext.stroke();
+		});
+	});
 }
